fb66a46df855b7946d00fe5f7988846e3be8650a
Manav Patil <manavpatil469@gmail.com>
2026-02-10 20:53:35 +0530

    addition program added in python

--- Java/patterns/p6.java
+++ Java/patterns/p6.java
@@ -1,9 +1,9 @@
-public class p4 {
+public class p6 {
     public static void main(String[] args) {
-        for(int i=1;i<=5;i++){
-            for(int j=1;j<=5;j++){
-                if(i==1 || j==1 || i==5 || j==5){
-                    System.out.print("*");
+        for(int i=1;i<=4;i++){
+            for(int j=1;j<=4;j++){
+                if(j<=i){
+                    System.out.print(j);
                 }else{
                     System.out.print(" ");
                 }
---
+++ Java/patterns/p8.java
@@ -0,0 +1,13 @@
+public class p8 {
+    public static void main(String[] args) {
+        for(int i=1;i<=4;i++){
+            for(int j=1;j<=4;j++){
+                if(j<=i){
+                    System.out.print((i+j+1)%2);
+                }else{
+                    System.out.print(" ");
+                }
+            }System.out.println();
+        }
+    }
+}
