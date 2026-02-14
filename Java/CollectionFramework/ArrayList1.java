6936fe1fea20f3cf85f21ce8e671e7f3dc2fe8e4
Manav Patil <manavpatil469@gmail.com>
2026-02-14 22:58:48 +0530

    collectionFramework updated code

---
+++ Java/CollectionFramework/ArrayList1.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+public class ArrayList1 {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+
+        System.out.println(list);
+
+        System.out.println("After removing element "+list.remove(1)+" : ");
+        System.out.println(list);
+
+    }
+}
