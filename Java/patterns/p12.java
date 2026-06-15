34b66793c9b363fc3ddd0da67fa762a4fdaecc08
Manav Patil <manavpatil469@gmail.com>
2026-06-15 18:14:10 +0530

    new patterns

--- Java/patterns/p12.java
+++ Java/patterns/p12.java
@@ -1,14 +1,36 @@
-class p12{
-    public static void main(String []args){
-        for(int i=0;i<=3;i++){
-            for(int j=0;j<=4;j++){
-                if(j>=4-i){
-                    System.out.print("*");
-                }else{
-                    System.out.print(" ");
-                }
+class p12 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            int spc = 2 * (5 - i);
+            for (int j = 1; j <= spc; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for (int i = 4; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            int spc = 2 * (5 - i);
+            for (int j = 1; j <= spc; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
             }
             System.out.println();
         }
     }
+
 }
