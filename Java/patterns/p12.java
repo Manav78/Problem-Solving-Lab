7082e52f9b7827b913fda5c8326586a708f4d3a7
Manav Patil <manavpatil469@gmail.com>
2026-06-15 12:29:53 +0530

    new code

---
+++ Java/patterns/p12.java
@@ -0,0 +1,14 @@
+class p12{
+    public static void main(String []args){
+        for(int i=0;i<=3;i++){
+            for(int j=0;j<=4;j++){
+                if(j>=4-i){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
