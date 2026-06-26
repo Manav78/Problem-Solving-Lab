50e8e45b527dc75720d444f3a80d8b6157acfea1
Manav78 <manavpatil469@gmail.com>
2026-06-26 13:31:18 +0530

    duplicates

---
+++ Java/Recursion/removeDuplicates.java
@@ -0,0 +1,22 @@
+package Recursion;
+
+public class removeDuplicates {
+    static boolean[] map = new boolean[26];
+
+    public static void removesDuplicate(String str, int idx, String newStr) {
+        if (str.length() == idx) {
+            System.out.println(newStr);
+            return;
+        }
+        if (map[str.codePointAt(idx)-'a']) {
+            removesDuplicate(str, idx+1, newStr);
+        } else {
+            newStr += str.charAt(idx);
+            map[str.charAt(idx) - 'a'] = true;
+            removesDuplicate(str, idx+1, newStr);
+        }
+    }
+    public static void main(String[] args) {
+        removesDuplicate("manavvam", 0, "");
+    }
+}
