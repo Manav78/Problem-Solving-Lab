331b5a8742a6fe037db03f65d581a26a4647bee3
Manav78 <manavpatil469@gmail.com>
2026-07-09 08:13:07 +0530

    bubble sort optimized version

--- Java/Sorting/bubbleSort.java
+++ Java/Sorting/bubbleSort.java
@@ -5,12 +5,17 @@ class bubbleSort {
         int arr[] = { 7, 8, 1, 3, 2 };
 
         for (int i = 0; i < arr.length - 1; i++) {
+            boolean isSkiped = true;
             for (int j = 0; j < arr.length - i - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
+                    isSkiped = false;
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
                 }
+            }
+            if (isSkiped) {
+                break;
             }
         }
 
---
+++ python/students.py
@@ -0,0 +1,18 @@
+#List
+Students=["Nianaa","Neha"]
+
+# Tuple
+subjects=("Java","HTML")
+
+# Set
+hobbies={"Eating","Reading"}
+
+student_dict={
+    101:{
+        "Name": Students[0],
+        "Subjects": subjects[1],
+        "Hobbies":list(hobbies)
+    }
+}
+
+print(student_dict)
